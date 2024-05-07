30be955ce8e650d9158df5c027c39a4420eddd77
Zahra Syyida <zahra.syyida@gmail.com>
2024-05-07 23:54:16 +0400

    Update Fwoosh.hpp

--- cpp_module01/Fwoosh.hpp
+++ cpp_module01/Fwoosh.hpp
@@ -8,5 +8,6 @@ class Fwoosh:public ASpell
 	public:
 	Fwoosh();
 	~Fwoosh();
+	// no need for =0 in child or virtual in cpp. Remember clone must use new bc it is kept seperately
 	virtual Fwoosh *clone() const;
 };
