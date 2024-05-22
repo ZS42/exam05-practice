83d699f95d13a3db8156c4b0e5388e507461df7e
Zahra Syyida <zahra.syyida@gmail.com>
2024-05-22 19:14:58 +0400

    Update Warlock.hpp

--- cpp_module01/Warlock.hpp
+++ cpp_module01/Warlock.hpp
@@ -20,6 +20,7 @@ class Warlock
 	const std::string &getTitle()const;
 	void setTitle(const std::string &title);
 	void learnSpell(ASpell *spell);
+// none of the new functions are constant
 	void forgetSpell(std::string spellName);
 	void launchSpell(std::string spellName, const ATarget &target);
 	void introduce() const;
